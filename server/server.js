d711596c6d86025c72c63c5b623c6a8da22471e4
Pablo Gutierrez <pablogtzgileta@gmail.com>
2019-02-07 18:29:37 -0600

    Setup separate test and development

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
